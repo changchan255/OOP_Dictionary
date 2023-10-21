b63e0a42164d52f1b0130c02660913a81d35028d
Le Van Hung <hungneit@gmail.com>
2023-10-21 11:52:23 +0700

    ab

--- Dictionary_Project/src/main/java/module-info.java
+++ Dictionary_Project/src/main/java/module-info.java
@@ -1,9 +1,9 @@
-module dictionaryapplication.dictionary_project {
+module App {
     requires javafx.controls;
     requires javafx.fxml;
 
     requires org.kordamp.bootstrapfx.core;
 
-    opens Controller.dictionary_project to javafx.fxml;
-    exports Controller.dictionary_project;
+    opens App to javafx.fxml;
+    exports App;
 }
