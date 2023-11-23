0cfc85153c77e04342ae3ca3bd0beb01aa8cd99d
changchan255 <thutrangg23252004@gmail.com>
2023-11-23 22:48:48 +0700

    fixed the spacetime hole

--- Dictionary_Project/src/main/java/App/SearchController.java
+++ Dictionary_Project/src/main/java/App/SearchController.java
@@ -2,8 +2,13 @@ package App;
 import DictionaryCmdLine.Dictionary;
 import DictionaryCmdLine.DictionaryManagement;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
